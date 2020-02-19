3524a1f788294ee0a355cac87c8fc311d84c8f50
RiceFamily <1079966197@qq.com>
2020-02-19 17:15:26 +0800

    将动态上传迁移到此处，在图片上传后 调用rest api进行动态上传

--- src/main/java/com/ricemarch/MinioController.java
+++ src/main/java/com/ricemarch/MinioController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +22,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 @Api(tags = "MinioController", description = "MinIO对象存储管理")
 @Controller
 @RequestMapping("/minio")
 public class MinioController {
+
+    @Autowired
+    UploadService uploadService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MinioController.class);
     @Value("${minio.endpoint}")
@@ -51,7 +56,7 @@ public class MinioController {
                 minioClient.makeBucket(BUCKET_NAME);
                 minioClient.setBucketPolicy(BUCKET_NAME, "*.*", PolicyType.READ_ONLY);
             }
-            String filename = file.getOriginalFilename();
+            String filename = UUID.randomUUID() + file.getOriginalFilename();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
             // 设置存储对象名称
             String objectName = sdf.format(new Date()) + "/" + filename;
@@ -88,7 +93,7 @@ public class MinioController {
             ArrayList<String> urls = new ArrayList<>();
             for (MultipartFile file : files
             ) {
-                String filename = file.getOriginalFilename();
+                String filename = UUID.randomUUID() + file.getOriginalFilename();
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
                 // 设置存储对象名称
                 String objectName = sdf.format(new Date()) + "/" + filename;
@@ -102,6 +107,53 @@ public class MinioController {
             minioMultUploadDto.setNames(filenames);
             minioMultUploadDto.setUrls(urls);
             return CommonResult.success(minioMultUploadDto);
+
+        } catch (Exception e) {
+            LOGGER.info("上传发生错误: {}！", e.getMessage());
+        }
+        return CommonResult.failed();
+    }
+
+
+    @ApiOperation("多个文件+信息上传")
+    @RequestMapping(value = "/uploadsInfo", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult upload_info(@RequestParam("files") ArrayList<MultipartFile> files,
+                                    @RequestParam String name,
+                                    @RequestParam String title,
+                                    @RequestParam String content) {
+        try {
+            MinioClient minioClient = new MinioClient(ENDPOINT, ACCESS_KEY, SECRET_KEY);
+            boolean isExist = minioClient.bucketExists(BUCKET_NAME);
+            if (isExist) {
+                LOGGER.info("存储桶已经存在！");
+            } else {
+                //创建存储桶并设置只读权限
+                minioClient.makeBucket(BUCKET_NAME);
+                minioClient.setBucketPolicy(BUCKET_NAME, "*.*", PolicyType.READ_ONLY);
+            }
+            MinioMultUploadDto minioMultUploadDto = new MinioMultUploadDto();
+            ArrayList<String> filenames = new ArrayList<>();
+            ArrayList<String> urls = new ArrayList<>();
+            for (MultipartFile file : files
+            ) {
+                String filename = UUID.randomUUID() + file.getOriginalFilename();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+                // 设置存储对象名称
+                String objectName = sdf.format(new Date()) + "/" + filename;
+                // 使用putObject上传一个文件到存储桶中
+                minioClient.putObject(BUCKET_NAME, objectName, file.getInputStream(), file.getContentType());
+                LOGGER.info("文件上传成功! user:" + name + " filename:" + filename);
+                filenames.add(filename);
+                urls.add(ENDPOINT + "/" + BUCKET_NAME + "/" + objectName);
+
+
+            }
+            uploadService.postMoment(title, content, name, urls);
+            minioMultUploadDto.setNames(filenames);
+            minioMultUploadDto.setUrls(urls);
+            return CommonResult.success(minioMultUploadDto);
+
         } catch (Exception e) {
             LOGGER.info("上传发生错误: {}！", e.getMessage());
         }
---
+++ src/main/java/com/ricemarch/UploadService.java
@@ -0,0 +1,52 @@
+package com.ricemarch;
+
+import okhttp3.*;
+import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class UploadService {
+
+
+    int postMoment(String title, String content, String username, ArrayList<String> imgUrls) {
+
+        String BASE_URL = "http://192.168.2.106:8081/moments/add";
+
+        int statusCode = 0;
+
+        OkHttpClient client = new OkHttpClient.Builder().connectTimeout(10000, TimeUnit.MICROSECONDS).build();
+
+        FormBody formBody = new FormBody.Builder()
+                .add("name", username)
+                .add("title", title)
+                .add("content", content)
+                .add("imgs", String.valueOf(imgUrls))
+                .build();
+
+        Request request = new Request.Builder()
+                .post(formBody)
+                .url(BASE_URL)
+                .build();
+        Call task = client.newCall(request);
+        task.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+//                Log.d(TAG, "onFailure " + e.toString());
+                System.out.println("onFailure " + e.toString());
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                System.out.println(response.protocol() + " " + response.code() + " " + response.message());
+                System.out.println("onResponse: " + response.body().toString());
+            }
+        });
+        return statusCode;
+    }
+}
